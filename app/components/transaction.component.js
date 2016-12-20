91d51869ad84716e7ea16db6a8df0c8a628cbe92
Rob Ede <robjtede@icloud.com>
2016-12-20 19:56:12 +0000

    iife scope transaction component

--- app/components/transaction.component.js
+++ app/components/transaction.component.js
@@ -1,124 +1,124 @@
 'use strict'
 
-const thisDoc = document.currentScript.ownerDocument
+;(function (thisDoc) {
+  document.addEventListener('DOMContentLoaded', () => {
+    // scope styles
+    window.ShadyCSS.prepareTemplate(thisDoc.querySelector('template'), 'm-transaction')
 
-document.addEventListener('DOMContentLoaded', () => {
-  // scope styles
-  window.ShadyCSS.prepareTemplate(thisDoc.querySelector('template'), 'm-transaction')
+    window.customElements.define('m-transaction', class extends HTMLElement {
+      constructor () {
+        super()
 
-  window.customElements.define('m-transaction', class extends HTMLElement {
-    constructor () {
-      super()
+        this.debug = false
+        if (this.debug) console.log('constructing')
 
-      this.debug = false
-      if (this.debug) console.log('constructing')
-
-      this.tx = {}
-    }
-
-    connectedCallback () {
-      if (this.debug) console.log(`connected ${this.index}`)
-
-      this.attachShadow({mode: 'open'})
-      this.root = this.shadowRoot
-
-      const template = thisDoc.querySelector('template')
-      this.root.appendChild(document.importNode(template.content, true))
-
-      window.ShadyCSS.applyStyle(this)
-
-      this.render()
-    }
-
-    render () {
-      if (this.debug) console.log(`rendering ${this.index}`)
-
-      this.root.querySelector('.description').textContent = this.description
-
-      if (this.notes.trim() !== '') {
-        this.root.querySelector('.notes').classList.add('noted')
-        this.root.querySelector('.notes').textContent = this.notes
+        this.tx = {}
       }
 
-      this.root.querySelector('.amount').textContent = this.amount
-      if (this.tx.amount >= 0) this.root.querySelector('.amount').classList.add('income')
+      connectedCallback () {
+        if (this.debug) console.log(`connected ${this.index}`)
 
-      this.root.querySelector('.icon').src = this.icon
+        this.attachShadow({mode: 'open'})
+        this.root = this.shadowRoot
 
-      if ('decline_reason' in this.tx) this.classList.add('declined')
-      if (!('settled' in this.tx)) this.classList.add('pending')
+        const template = thisDoc.querySelector('template')
+        this.root.appendChild(document.importNode(template.content, true))
 
-      this.dataset.category = this.tx.category
-    }
+        window.ShadyCSS.applyStyle(this)
 
-    get index () {
-      return this.dataset.index
-    }
-
-    get description () {
-      if ('merchant' in this.tx && this.tx.merchant && this.tx.merchant.name) {
-        return this.tx.merchant.name
-      } else return this.tx.description
-    }
-
-    get notes () {
-      return this.tx.notes.split('\n')[0]
-    }
-
-    get amount () {
-      let amount = this.tx.amount / 100
-
-      if (amount < 0) amount = `${Math.abs(amount).toFixed(2)}`
-      else amount = `+${amount.toFixed(2)}`
-
-      return amount
-    }
-
-    formatCurrency (amount) {
-      const currencies = {
-        'GBP': '£',
-        'USD': '$',
-        'EUR': '€'
+        this.render()
       }
 
-      return `${currencies[this.tx.currency] || ''}${amount}`
-    }
+      render () {
+        if (this.debug) console.log(`rendering ${this.index}`)
 
-    get icon () {
-      if ('is_topup' in this.tx.metadata && this.tx.metadata.is_topup) {
-        return './icons/topup.png'
+        this.root.querySelector('.description').textContent = this.description
+
+        if (this.notes.trim() !== '') {
+          this.root.querySelector('.notes').classList.add('noted')
+          this.root.querySelector('.notes').textContent = this.notes
+        }
+
+        this.root.querySelector('.amount').textContent = this.amount
+        if (this.tx.amount >= 0) this.root.querySelector('.amount').classList.add('income')
+
+        this.root.querySelector('.icon').src = this.icon
+
+        if ('decline_reason' in this.tx) this.classList.add('declined')
+        if (!('settled' in this.tx)) this.classList.add('pending')
+
+        this.dataset.category = this.tx.category
       }
 
-      if (this.tx.counterparty && 'user_id' in this.tx.counterparty) {
-        return './icons/peer.png'
+      get index () {
+        return this.dataset.index
       }
 
-      if ('merchant' in this.tx && this.tx.merchant && 'logo' in this.tx.merchant && this.tx.merchant.logo) {
-        return this.tx.merchant.logo
+      get description () {
+        if ('merchant' in this.tx && this.tx.merchant && this.tx.merchant.name) {
+          return this.tx.merchant.name
+        } else return this.tx.description
       }
 
-      return `./icons/${this.tx.category}.png`
-    }
+      get notes () {
+        return this.tx.notes.split('\n')[0]
+      }
 
-    disconnectedCallback () {
-      if (this.debug) console.log(`disconnection ${this.index}`)
-    }
+      get amount () {
+        let amount = this.tx.amount / 100
 
-    adoptedCallback () {
-      if (this.debug) console.log(`adopted ${this.index}`)
-    }
+        if (amount < 0) amount = `${Math.abs(amount).toFixed(2)}`
+        else amount = `+${amount.toFixed(2)}`
 
-    static get observedAttributes () {
-      return []
-    }
+        return amount
+      }
 
-    attributeChangedCallback (attrName, oldVal, newVal) {
-      if (this.debug) console.log(`attribute changed on ${this.index}: ${attrName}, ${oldVal} => ${newVal}`)
-      const changes = {}
+      formatCurrency (amount) {
+        const currencies = {
+          'GBP': '£',
+          'USD': '$',
+          'EUR': '€'
+        }
 
-      if (attrName in changes) changes[attrName]()
+        return `${currencies[this.tx.currency] || ''}${amount}`
+      }
 
-      this.render()
-    }
+      get icon () {
+        if ('is_topup' in this.tx.metadata && this.tx.metadata.is_topup) {
+          return './icons/topup.png'
+        }
+
+        if (this.tx.counterparty && 'user_id' in this.tx.counterparty) {
+          return './icons/peer.png'
+        }
+
+        if ('merchant' in this.tx && this.tx.merchant && 'logo' in this.tx.merchant && this.tx.merchant.logo) {
+          return this.tx.merchant.logo
+        }
+
+        return `./icons/${this.tx.category}.png`
+      }
+
+      disconnectedCallback () {
+        if (this.debug) console.log(`disconnection ${this.index}`)
+      }
+
+      adoptedCallback () {
+        if (this.debug) console.log(`adopted ${this.index}`)
+      }
+
+      static get observedAttributes () {
+        return []
+      }
+
+      attributeChangedCallback (attrName, oldVal, newVal) {
+        if (this.debug) console.log(`attribute changed on ${this.index}: ${attrName}, ${oldVal} => ${newVal}`)
+        const changes = {}
+
+        if (attrName in changes) changes[attrName]()
+
+        this.render()
+      }
+    })
   })
-})
+})(document.currentScript.ownerDocument)
