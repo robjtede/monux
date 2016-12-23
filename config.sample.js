dc887e1a44fce4ea3ba0520083487764e836cd98
Rob Ede <robjtede@icloud.com>
2016-12-23 23:44:58 +0000

    prepare 0.2.0 release

--- config.sample.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-module.export = {
-  client_id: 'oauthclient_0000000000000000000000',
-  client_secret: 'a0000000000000000000000',
-  state: 'maybe randomly generate this during each oauth'
-}
