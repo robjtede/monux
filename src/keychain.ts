b48874301e1feb392f25632b2a43d344d1d2cf1c
Rob Ede <robjtede@icloud.com>
2017-06-15 23:25:33 +0100

    abstract keychain functions

---
+++ src/keychain.ts
@@ -0,0 +1,28 @@
+import * as keychain from 'keychain'
+
+export const getPassword = (opts: IKeychain): Promise<string> => {
+  return new Promise((resolve, reject) => {
+    keychain.getPassword(opts, (err: Error | null, pass: string) => {
+      if (err) reject(err)
+      resolve(pass)
+    })
+  })
+}
+
+export const setPassword = (opts: IKeychainSet) => {
+  return new Promise((resolve, reject) => {
+    keychain.setPassword(opts, (err: Error | null) => {
+      if (err) reject(err)
+      resolve()
+    })
+  })
+}
+
+export const deletePassword = (opts: IKeychain) => {
+  return new Promise((resolve, reject) => {
+    keychain.deletePassword(opts, (err: Error | null) => {
+      if (err) reject(err)
+      resolve()
+    })
+  })
+}
