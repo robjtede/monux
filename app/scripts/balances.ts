2fee8ae23879132a5045c56c142a965512119491
Rob Ede <robjtede@icloud.com>
2017-07-08 23:21:58 +0100

    refactor balances

--- app/scripts/balances.ts
+++ app/scripts/balances.ts
@@ -1,8 +1,8 @@
-import { Amount, Monzo } from '../../lib/monzo'
+import { Account, Amount, Monzo } from '../../lib/monzo'
 import { getSavedCode } from '../../lib/monzo/auth'
 
 import setTouchBar from './touchbar'
-import cache from './cache'
+import cache, { ICacheBank } from './cache'
 
 const getMonzo = (() => {
   const accessToken = getSavedCode('access_token')
@@ -11,6 +11,32 @@ const getMonzo = (() => {
     return new Monzo(await accessToken)
   }
 })()
+
+export const getCachedBank = (() => {
+  const cachedBank = cache.banks.limit(1).toArray()
+
+  return async (): Promise<ICacheBank> => {
+    return (await cachedBank)[0]
+  }
+})()
+
+export const getCachedBalance = (() => {
+  const cachedBank = getCachedBank()
+
+  return async (): Promise<Amount> => {
+    const { native, local } = JSON.parse((await cachedBank).balance)
+    return new Amount(native, local)
+  }
+})()
+
+export const updateAccountCache = async (acc: Account, balance: Amount) => {
+  return cache.banks.put({
+    accId: acc.id,
+    balance: balance.json,
+    name: acc.name,
+    type: 'Monzo'
+  })
+}
 
 document.addEventListener('DOMContentLoaded', async () => {
   const $header = document.querySelector('header') as HTMLElement
@@ -23,24 +49,36 @@ document.addEventListener('DOMContentLoaded', async () => {
   ) as HTMLHeadingElement
   const $accDescription = $nav.querySelector('.person') as HTMLDivElement
 
-  const accounts = await (await getMonzo()).accounts
+  const updateBalances = (balance?: Amount, spentToday?: Amount) => {
+    if ((balance && !balance.foreign) || (spentToday && !spentToday.foreign)) {
+      if (balance) $balance.innerHTML = balance.html(true, 0)
+      if (spentToday) $spentToday.innerHTML = spentToday.html(true, 0)
+    } else {
+      if (balance) {
+        $balance.innerHTML =
+          (balance.exchanged as Amount).html(true, 0) + balance.html(true, 0)
+      }
+      if (spentToday) {
+        $spentToday.innerHTML =
+          (spentToday.exchanged as Amount).html(true, 0) +
+          spentToday.html(true, 0)
+      }
+    }
+
+    setTouchBar(balance, spentToday)
+  }
+
+  const updateAccountInfo = (account: ICacheBank | Account) => {
+    $accDescription.textContent = account.name
+  }
 
   console.time('render cached balance')
   try {
-    const cachedBank = (await cache.banks.limit(1).toArray())[0]
-    const { native, local } = JSON.parse(cachedBank.balance)
-    const balance = new Amount(native, local)
+    const cachedBank = await getCachedBank()
+    updateAccountInfo(cachedBank)
 
-    $accDescription.textContent = cachedBank.name
-
-    if (!balance.foreign) {
-      $balance.innerHTML = balance.html(true, 0)
-    } else {
-      $balance.innerHTML =
-        (balance.exchanged as Amount).html(true, 0) + balance.html(true, 0)
-    }
-
-    setTouchBar(balance)
+    const cachedBalance = await getCachedBalance()
+    updateBalances(cachedBalance)
   } catch (err) {
     console.error(err)
   }
@@ -48,30 +86,12 @@ document.addEventListener('DOMContentLoaded', async () => {
 
   console.time('render HTTP balance')
   try {
-    const acc = accounts[0]
+    const acc = (await (await getMonzo()).accounts)[0]
     const { balance, spentToday } = await acc.balance
 
-    await cache.banks.put({
-      accId: acc.id,
-      balance: balance.json,
-      name: acc.name,
-      type: 'Monzo'
-    })
-
-    $accDescription.textContent = acc.name
-
-    if (!balance.foreign) {
-      $balance.innerHTML = balance.html(true, 0)
-      $spentToday.innerHTML = spentToday.html(true, 0)
-    } else {
-      $balance.innerHTML =
-        (balance.exchanged as Amount).html(true, 0) + balance.html(true, 0)
-      $spentToday.innerHTML =
-        (spentToday.exchanged as Amount).html(true, 0) +
-        spentToday.html(true, 0)
-    }
-
-    setTouchBar(balance, spentToday)
+    updateAccountCache(acc, balance)
+    updateAccountInfo(acc)
+    updateBalances(balance, spentToday)
   } catch (err) {
     console.error(err)
   }
