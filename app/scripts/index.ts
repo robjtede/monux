d25baf4bb529795b2785556fa23c031131609c7d
Rob Ede <robjtede@icloud.com>
2017-07-27 02:07:05 +0100

    add devtron

--- app/scripts/index.ts
+++ app/scripts/index.ts
@@ -1,5 +1,8 @@
+import devtron = require('devtron')
 import context = require('electron-contextmenu-middleware')
 import imageMenu = require('electron-image-menu')
+
+devtron.install()
 
 context.use(imageMenu)
 context.activate()
