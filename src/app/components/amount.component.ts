14ae83537b8124d2f5c61413fe09e37fac21549a
Rob Ede <robjtede@icloud.com>
2017-08-20 03:46:41 +0100

    added currency modes

--- src/app/components/amount.component.ts
+++ src/app/components/amount.component.ts
@@ -15,20 +15,20 @@ export class AmountComponent {
   @Input() readonly amount: Amount
   @Input() readonly show?: ShowModes = 'both'
   @Input('sign-mode') readonly signMode?: SignModes = 'always'
-  @Input('show-currency') readonly showCurrency?: boolean = true
+  @Input('currency-mode') readonly currencyMode?: CurrencyModes = 'both'
   @Input('large-major') readonly largeMajor?: boolean = false
 
   get showNative() {
-    return !this.show || this.show === 'both' || this.show === 'native'
+    return this.show === 'both' || this.show === 'native'
   }
 
   get showLocal() {
     return (
-      this.amount.foreign &&
-      (!this.show || this.show === 'both' || this.show === 'local')
+      this.amount.foreign && (this.show === 'both' || this.show === 'local')
     )
   }
 }
 
 export type SignModes = 'always' | 'onlyPositive' | 'onlyNegative' | 'never'
-export type ShowModes = 'both' | 'local' | 'native'
+export type ShowModes = 'both' | 'local' | 'native' | 'neither'
+export type CurrencyModes = 'both' | 'local' | 'native' | 'neither'
