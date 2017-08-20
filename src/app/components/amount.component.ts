95e828029646fc6bcac0ba3c7a292baf2623c26a
Rob Ede <robjtede@icloud.com>
2017-08-20 03:09:31 +0100

    remove innerHTML usage from amount component

--- src/app/components/amount.component.ts
+++ src/app/components/amount.component.ts
@@ -1,23 +1,34 @@
 import { Component, Input, ChangeDetectionStrategy } from '@angular/core'
-import { Amount, SignModes } from '../../lib/monzo/Amount'
+import { Amount } from '../../lib/monzo/Amount'
 
 @Component({
   selector: 'm-amount',
   templateUrl: './amount.component.html',
   styleUrls: ['./amount.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '[class.positive]': 'amount.positive',
+    '[class.negative]': 'amount.negative'
+  }
 })
 export class AmountComponent {
   @Input() readonly amount: Amount
+  @Input() readonly show?: ShowModes = 'both'
+  @Input('sign-mode') readonly signMode?: SignModes = 'always'
+  @Input('show-currency') readonly showCurrency?: boolean = true
+  @Input('large-major') readonly largeMajor?: boolean = false
 
-  getAmountHtml() {
-    return !this.amount.foreign
-      ? this.amount.html({
-          signMode: SignModes.Never
-        })
-      : (this.amount.exchanged as Amount).html({ signMode: SignModes.Never }) +
-        this.amount.html({
-          signMode: SignModes.Never
-        })
+  get showNative() {
+    return !this.show || this.show === 'both' || this.show === 'native'
+  }
+
+  get showLocal() {
+    return (
+      this.amount.foreign &&
+      (!this.show || this.show === 'both' || this.show === 'local')
+    )
   }
 }
+
+export type SignModes = 'always' | 'onlyPositive' | 'onlyNegative' | 'never'
+export type ShowModes = 'both' | 'local' | 'native'
