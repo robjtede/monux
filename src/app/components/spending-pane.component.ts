f80832ac35dc2e9df51fa2d6ac155239e8215705
Rob Ede <robjtede@icloud.com>
2018-06-25 23:55:43 +0100

    fix inconsistent chart sizing
    closes #141

--- src/app/components/spending-pane.component.ts
+++ src/app/components/spending-pane.component.ts
@@ -1,53 +1,84 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core'
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  AfterContentInit,
+  ViewChild
+} from '@angular/core'
 import { Store } from '@ngrx/store'
-import { schemeSet1 } from 'd3'
-import { AppState } from '../store'
+import { Observable } from 'rxjs'
+import { map } from 'rxjs/operators'
+import { schemeCategory10 as colorScheme } from 'd3'
+import { PieChartComponent } from '@swimlane/ngx-charts'
 
-import { txdata } from '../txdata'
+import { AppState } from '../store'
 import { Transaction } from '../../lib/monzo/Transaction'
 import { GroupingStrategy, groupTransactions } from '../../lib/monzo/helpers'
 
 @Component({
   selector: 'm-spending-pane',
-  template: `
-    <div class="spending-vis" style="flex: 1;">
-      <ngx-charts-pie-chart
-        [results]="data"
-        [doughnut]="true"
-        [legend]="true"
-        [legendTitle]="legendTitle"
-        [scheme]="colorScheme"
-        [labels]="true"
-        [trimLabels]="false"
-      ></ngx-charts-pie-chart>
-    </div>
-  `,
+  templateUrl: './spending-pane.component.html',
+  styleUrls: ['./spending-pane.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   host: {
     '[class.pane]': 'true'
   }
 })
-export class SpendingPaneComponent implements OnInit {
-  legendTitle = 'This Month'
+export class SpendingPaneComponent implements OnInit, AfterContentInit {
+  @ViewChild(PieChartComponent) $chart!: PieChartComponent
 
-  data = groupTransactions(
-    txdata.map((tx: any) => new Transaction(tx)),
-    GroupingStrategy.Category
-  ).map(cat => {
-    return {
-      name: cat.id,
-      value: cat.txs.reduce(
-        (sum, tx) => sum + (tx.amount.negative ? tx.amount.amount : 0),
-        0
-      )
-    }
-  })
+  legendTitle = 'This Month'
+  data!: Observable<TooltipData[]>
 
   colorScheme = {
-    domain: schemeSet1
+    domain: colorScheme
   }
 
   constructor(private store$: Store<AppState>) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.data = this.store$.select('transactions').pipe(
+      map(txs => {
+        return groupTransactions(
+          txs.map((tx: any) => new Transaction(tx)),
+          GroupingStrategy.Category
+        ).map(cat => {
+          return {
+            name: cat.id,
+            value: cat.txs.reduce(
+              (sum, tx) => sum + (tx.amount.negative ? tx.amount.amount : 0),
+              0
+            )
+          }
+        })
+      })
+    )
+  }
+
+  ngAfterContentInit() {
+    // HACK: for consistent graph sizing due to container dimensions
+    // not updated in time
+    setTimeout(() => {
+      this.$chart.update()
+    }, 0)
+  }
+
+  ttt(params: TooltipTextParams) {
+    return `£${params.data.value.toFixed(2)}`
+  }
+}
+
+interface TooltipData {
+  name: string
+  value: number
+}
+
+interface TooltipTextParams {
+  data: TooltipData
+  endAngle: number
+  index: number
+  padAngle: number
+  pos: number[]
+  startAngle: number
+  value: number
 }
