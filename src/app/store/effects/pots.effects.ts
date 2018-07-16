56014c3806f6169af678c6a8eaf5a810144a2e72
Rob Ede <robjtede@icloud.com>
2018-07-16 21:09:57 +0100

    cache pot responses

--- src/app/store/effects/pots.effects.ts
+++ src/app/store/effects/pots.effects.ts
@@ -3,8 +3,15 @@ import { Actions, Effect, ofType } from '@ngrx/effects'
 import { Action, Store } from '@ngrx/store'
 import Debug = require('debug')
 import { MonzoPotsResponse, potsRequest } from 'monzolib'
-import { concat, defer, Observable, of } from 'rxjs'
-import { catchError, map, switchMap, switchMapTo, tap } from 'rxjs/operators'
+import { concat, defer, Observable, of, combineLatest, empty } from 'rxjs'
+import {
+  catchError,
+  map,
+  switchMap,
+  switchMapTo,
+  tap,
+  filter
+} from 'rxjs/operators'
 
 import { AppState } from '..'
 import { CacheService } from '../../services/cache.service'
@@ -13,7 +20,8 @@ import {
   GET_POTS,
   SetPotsAction,
   GetPotsFailedAction,
-  GetPotsAction
+  GetPotsAction,
+  SET_POTS
 } from '../actions/pots.actions'
 
 const debug = Debug('app:effects:pots')
@@ -28,52 +36,43 @@ export class PotsEffects {
   ) {}
 
   @Effect()
-  get$: Observable<Action> = this.actions$.pipe(
+  getAll$: Observable<Action> = this.actions$.pipe(
     ofType(GET_POTS),
     switchMap(() =>
       // stream of cached pots then http pots
       concat(
-        // this.cache.loadpots(acc.id).pipe(
-        //   map(({ pots }) => pots),
-        //   tap(pots => {
-        //     debug('cached pots:', pots)
-        //   }),
-        //   catchError(err => {
-        //     console.error(err)
-        //     return empty()
-        //   })
-        // ),
-        this.monzo.request<MonzoPotsResponse>(potsRequest()).pipe(
+        this.cache.loadPots().pipe(
+          map(pots => pots.map(({ pot }) => pot)),
           tap(pots => {
-            debug('http pots', pots)
+            debug(pots.length, 'cached pots:', pots)
+          }),
+          catchError(err => {
+            console.error(err)
+            return empty()
+          })
+        ),
+        this.monzo.request<MonzoPotsResponse>(potsRequest()).pipe(
+          map(({ pots }) => pots),
+          tap(pots => {
+            debug(pots.length, 'http pots', pots)
           })
         )
       )
     ),
-    map(({ pots }) => new SetPotsAction(pots)),
+    map(pots => new SetPotsAction(pots)),
     catchError(err => {
       console.error(err)
       return of(new GetPotsFailedAction())
     })
   )
 
-  // @Effect({ dispatch: false })
-  // savepots$: Observable<any> = this.actions$.pipe(
-  //   ofType(SET_pots),
-  //   switchMap((action: SetpotsAction) =>
-  //     combineLatest(
-  //       this.store$.select('account').pipe(filter(acc => !!acc)),
-  //       of(action.payload)
-  //     )
-  //   ),
-  //   switchMap(([account, potsRes]) => {
-  //     if (account) {
-  //       return this.cache.savepots(account.id, potsRes)
-  //     } else {
-  //       throw new Error('cannot save pots of account that doesnt exist')
-  //     }
-  //   })
-  // )
+  @Effect({ dispatch: false })
+  save$: Observable<any> = this.actions$.pipe(
+    ofType(SET_POTS),
+    switchMap((action: SetPotsAction) => {
+      return this.cache.savePots(action.payload)
+    })
+  )
 
   @Effect()
   init$: Observable<Action> = this.actions$.pipe(
